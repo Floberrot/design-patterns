8eb5c9c0489663c0e9d620dd386899be22604716
floberrot <florian.berrot92@gmail.com>
2025-07-17 10:50:13 +0200

    :zap: improve flyweight tests

--- src/Patterns/Structural/Flyweight/BadgeType.php
+++ src/Patterns/Structural/Flyweight/BadgeType.php
@@ -7,8 +7,10 @@ class BadgeType
     public function __construct(
         private readonly BadgeNameEnum  $name,
         private readonly BadgeColorEnum $color,
+        private string                  $bigData = ''
     )
     {
+        $this->bigData = str_repeat('x', 5000);
     }
 
     public function getName(): string
